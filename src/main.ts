c5dd0da5a4e73fa52e1ff203f2f12584f28359c4
ryoya ito <horusu.dqx@gmail.com>
2020-05-16 23:56:14 +0900

    test

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,9 @@ async function run(): Promise<void> {
     const ms: string = core.getInput('milliseconds')
     core.debug(`Waiting ${ms} milliseconds ...`)
 
-    core.debug(new Date().toTimeString())
+    core.info(new Date().toTimeString())
     await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    core.info(new Date().toTimeString())
 
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {

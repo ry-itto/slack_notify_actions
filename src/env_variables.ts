c81222427d70f38a4fa9086d97471c3bdd51c6f7
ryoya ito <horusu.dqx@gmail.com>
2020-05-17 18:40:57 +0900

    commonly

---
+++ src/env_variables.ts
@@ -0,0 +1,52 @@
+interface GitHubEnvVariables {
+  githubActor: string
+  githubRef: string
+  githubEvent: string
+}
+
+interface ProcessEnvVariables {
+  webhookURL: string
+  slackIconURL?: string
+  slackUsername?: string
+  attachmentsTitle?: string
+  attachmentsTitleURL?: string
+  attachmentsBody?: string
+  attachmentsColor: string
+}
+
+export const readEnvVariables = (): EnvVariables => {
+  const webhookURL = process.env.SLACK_WEBHOOK_URL
+  const githubActor = process.env.GITHUB_ACTOR
+  const githubRef = process.env.GITHUB_REF
+  const githubEvent = process.env.GITHUB_EVENT_NAME
+
+  if (!webhookURL) {
+    throw Error(
+      `Env variable SLACK_WEBHOOK_URL not found. Please specify this variable.`
+    )
+  }
+
+  if (!(githubActor && githubRef && githubEvent)) {
+    throw Error(`
+      Variable(s) defined by GitHub are not found.
+      GITHUB_ACTOR: ${githubActor},
+      GITHUB_REF: ${githubRef},
+      GITHUB_EVENT_NAME: ${githubEvent},
+    `)
+  }
+
+  return {
+    webhookURL,
+    githubActor,
+    githubRef,
+    githubEvent,
+    slackIconURL: process.env.SLACK_ICON_URL,
+    slackUsername: process.env.SLACK_USERNAME,
+    attachmentsTitle: process.env.TITLE ?? '',
+    attachmentsTitleURL: process.env.TITLE_URL ?? '',
+    attachmentsBody: process.env.BODY ?? '',
+    attachmentsColor: process.env.COLOR ?? 'green'
+  }
+}
+
+export type EnvVariables = GitHubEnvVariables & ProcessEnvVariables
--- src/main.ts
+++ src/main.ts
@@ -2,49 +2,40 @@ import * as core from '@actions/core'
 import * as slack from '@slack/webhook'
 import {MessageAttachment} from '@slack/types'
 import {createAttachment, createIncomingWebhookSendArguments} from './utils'
+import {readEnvVariables} from './env_variables'
 
-type Optional<T> = T | null | undefined
 async function run(): Promise<void> {
-  const webhookURL: string = process.env.SLACK_WEBHOOK_URL!
-  const githubActor: string = process.env.GITHUB_ACTOR!
-  const githubRef: string = process.env.GITHUB_REF!
-  const githubEvent: string = process.env.GITHUB_EVENT_NAME!
-  const slackIconURL: Optional<string> = process.env.SLACK_ICON_URL
-  const slackUsername: Optional<string> = process.env.SLACK_USERNAME
-  const attachmentsTitle: string = process.env.TITLE ?? ''
-  const attachmentsTitleURL: Optional<string> = process.env.TITLE_URL ?? ''
-  const attachmentsBody: string = process.env.BODY ?? ''
-  const attachmentsColor: string = process.env.COLOR ?? 'green'
+  const envVariables = readEnvVariables()
 
-  const webhook = new slack.IncomingWebhook(webhookURL)
+  const webhook = new slack.IncomingWebhook(envVariables.webhookURL)
   const attachments: MessageAttachment = createAttachment({
-    color: attachmentsColor,
-    authorName: githubActor,
-    authorLink: `https://github.com/${githubActor}`,
-    authorIcon: `https://github.com/${githubActor}.png`,
-    title: attachmentsTitle,
-    titleLink: attachmentsTitleURL,
+    color: envVariables.attachmentsColor,
+    authorName: envVariables.githubActor,
+    authorLink: `https://github.com/${envVariables.githubActor}`,
+    authorIcon: `https://github.com/${envVariables.githubActor}.png`,
+    title: envVariables.attachmentsTitle,
+    titleLink: envVariables.attachmentsTitleURL,
     fields: [
       {
         title: 'Ref',
-        value: githubRef,
+        value: envVariables.githubRef,
         short: true
       },
       {
         title: 'Event',
-        value: githubEvent,
+        value: envVariables.githubEvent,
         short: true
       },
       {
         title: '',
-        value: attachmentsBody,
+        value: envVariables.attachmentsBody ?? '',
         short: false
       }
     ]
   })
   const args = createIncomingWebhookSendArguments({
-    iconUrl: slackIconURL,
-    username: slackUsername,
+    iconUrl: envVariables.slackIconURL,
+    username: envVariables.slackUsername,
     attachments: [attachments]
   })
 
@@ -57,4 +48,6 @@ async function run(): Promise<void> {
   }
 }
 
-run()
+run().catch(err => {
+  core.setFailed(err.message)
+})
